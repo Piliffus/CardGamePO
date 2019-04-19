57912f88ded82c2eb4f7fc95159d7c136295094e
Filip Bienkowski <bienkowskifilip@gmail.com>
2019-04-19 22:47:08 +0200

    Added TODOs and ran some tests

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main
 {
+    // TODO Manual input wrong simulations - money, stakes input > int, wrong inputs at money and stakes,
+    //  stakes larger than money or too small
+    //  buffer overflow (to many rounds) - hard limit and too big difference between mon and stk, money <= 0, stakes <= 0,
+
+
+    // TODO better shuffling???
+    // TODO tools for running large numbers of simulations
 
     public static void main(String[] args)
     {
