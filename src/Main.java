ddc4e4ef4980feaad237801e4b1976801861d9a3
Filip Bienkowski <bienkowskifilip@gmail.com>
2019-04-20 13:53:39 +0200

    Minor changes

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 public class Main
 {
-    //  buffer overflow (to many rounds) - hard limit
-    // running a lot of sims
-
     public static void main(String[] args)
     {
         GameController gameController = new GameController();
